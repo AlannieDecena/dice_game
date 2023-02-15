bef5ab26248ba09cd88008a8cbf35349bb69c4f4
alanniedecena <alannie.khem.decena@gmail.com>
2023-02-15 19:28:10 +1300

    6&10.working

--- dice.js
+++ dice.js
@@ -13,7 +13,10 @@
 
 function rollDice6 () {
     dice6 = Math.floor((Math.random()*6+1));
+    dice10 = Math.floor((Math.random()*10+1));
     
     document.getElementById("dicelable").innerHTML = dice6;
-
+    document.getElementById("dicelable").innerHTML = dice10;
 }
+
+
